0a4105f5928b58ab513689f6d12d5a9a6c4ea66a
polanty <Tijaniabiola1599@gmail.com>
2025-04-20 16:26:54 -0700

    added the iphone slide animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import Highlights from './components/Highlights';
-import Model from './components/Model';
-import Features from './components/Features';
-import HowItWorks from './components/HowItWorks';
-import Footer from './components/Footer';
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Highlights from "./components/Highlights";
+import Model from "./components/Model";
 
-import * as Sentry from '@sentry/react';
+import * as Sentry from "@sentry/react";
 
 const App = () => {
   return (
@@ -15,11 +12,8 @@ const App = () => {
       <Hero />
       <Highlights />
       <Model />
-      <Features />
-      <HowItWorks />
-      <Footer />
     </main>
-  )
-}
+  );
+};
 
 export default Sentry.withProfiler(App);
